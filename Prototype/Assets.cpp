d6ae15801b9622b9bff35dccb2a4a35b16c97580
rockets4fun <rockets4fun@users.noreply.github.com>
2015-01-15 23:34:59 +0100

    Basic assets infrastructure for models

--- Prototype/Assets.cpp
+++ Prototype/Assets.cpp
@@ -11,15 +11,96 @@
 // -------------------------------------------------------------------------------------------------
 Assets::Assets()
 {
-    Assimp::Importer importer;
-    const aiScene *scene = importer.ReadFile("Assets/Sphere.obj", 0);
-    if (!scene)
-    {
-        // ...
-    }
 }
 
 // -------------------------------------------------------------------------------------------------
 Assets::~Assets()
 {
+}
+
+// -------------------------------------------------------------------------------------------------
+u32 Assets::asset(const std::string &name)
+{
+    u32 hash = krHash(name.c_str(), name.length());
+    auto infoIter = m_assetInfos.find(hash);
+    if (infoIter != m_assetInfos.end())
+    {
+        // Check for hash function collision
+        COMMON_ASSERT(infoIter->second.name == name);
+    }
+    else
+    {
+        Info &info = m_assetInfos[hash];
+        info.name = name;
+        info.hash = hash;
+    }
+    return hash;
+}
+
+// -------------------------------------------------------------------------------------------------
+u32 Assets::assetVersion(u32 hash)
+{
+    auto infoIter = m_assetInfos.find(hash);
+    if (infoIter == m_assetInfos.end())
+    {
+        return 0;
+    }
+    return infoIter->second.version;
+}
+
+// -------------------------------------------------------------------------------------------------
+const Assets::Model *Assets::refModel(u32 hash)
+{
+    // TODO(martinmo): Generalize this into a template function for all asset types
+    auto infoIter = m_assetInfos.find(hash);
+    if (infoIter == m_assetInfos.end())
+    {
+        return nullptr;
+    }
+    Info &info = infoIter->second;
+    if (info.type != Info::Type::UNDEFINED && info.type != Info::Type::MODEL)
+    {
+        return nullptr;
+    }
+    Model &model = m_models[hash];
+    if (info.type == Info::Type::UNDEFINED)
+    {
+        // This function triggers a load only the first time it touches an asset
+        if (loadModel(info, model))
+        {
+            ++info.version;
+        }
+        info.type = Info::Type::MODEL;
+    }
+    if (info.version == 0)
+    {
+        // TODO(martinmo): Return default model data if we failed to load (e.g. unit cube)
+    }
+    return &model;
+}
+
+// -------------------------------------------------------------------------------------------------
+u32 Assets::krHash(const char *data, size_t size)
+{
+    // Kernigan & Ritchie "The C Programming Language" hash
+    u32 hash = 0;
+    while (size-- > 0)
+    {
+        hash = (hash * 131 + *data++) & 0xffffffff;
+    }
+    return hash;
+}
+
+// -------------------------------------------------------------------------------------------------
+bool Assets::loadModel(Info &info, Model &model)
+{
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile(info.name, 0);
+    if (!scene)
+    {
+        // FIXME(martinmo): Proper error message through platform abstraction
+        return false;
+    }
+
+    return true;
 }
--- Prototype/Assets.hpp
+++ Prototype/Assets.hpp
@@ -8,12 +8,53 @@
 
 #include "Common.hpp"
 
+#include <map>
+#include <vector>
+
 // -------------------------------------------------------------------------------------------------
-/// @brief Brief implementation module description
+/// @brief Application assets handling
 struct Assets
 {
+    struct Info
+    {
+        enum Type
+        {
+            UNDEFINED,
+            MODEL
+        };
+        u32 hash;
+        std::string name;
+        Type type = UNDEFINED;
+        u32 version = 0;
+    };
+
+    struct Model
+    {
+        // Positions and normals for triangle vertices
+        std::vector< float > positions;
+        std::vector< float > normals;
+
+        // TODO(martinmo): Think about restructuring into meshes storing
+        // TODO(martinmo): - Separate name and hash
+        // TODO(martinmo): - Primitive type (others than just triangles...)
+        // TODO(martinmo): - Indices into actual mesh data stored in model?
+    };
+
     Assets();
     virtual ~Assets();
+
+    u32 asset(const std::string &name);
+    u32 assetVersion(u32 hash);
+
+    const Model *refModel(u32 hash);
+
+private:
+    std::map< u32, Info > m_assetInfos;
+    std::map< u32, Model > m_models;
+
+    static bool loadModel(Info &info, Model &model);
+
+    u32 krHash(const char *data, size_t size);
 
 private:
     COMMON_DISABLE_COPY(Assets);
