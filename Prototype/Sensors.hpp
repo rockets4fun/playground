f0e57d5e36e3684ff5bcb7718846a2f9cf623ca9
rockets4fun <rockets4fun@users.noreply.github.com>
2018-04-24 14:49:45 +0200

    Add prototype for sensors API

---
+++ Prototype/Sensors.hpp
@@ -0,0 +1,33 @@
+// -------------------------------------------------------------------------------------------------
+/// @author Martin Moerth (MARTINMO)
+/// @date 15.03.2017
+// -------------------------------------------------------------------------------------------------
+
+#ifndef SENSORS_HPP
+#define SENSORS_HPP
+
+#include "Common.hpp"
+
+#include "ModuleIf.hpp"
+
+// -------------------------------------------------------------------------------------------------
+/// @brief Sensors module
+struct Sensors : public ModuleIf
+{
+    Sensors();
+    virtual ~Sensors();
+
+    static void updateAccelerometer(Sensor::Info &sensor, glm::fvec4  accelLinear);
+    static void updateGyro         (Sensor::Info &sensor, glm::fvec4  accelAngular);
+
+public: // Implementation of module interface
+    virtual void registerTypesAndStates(StateDb &sdb);
+    virtual bool initialize(StateDb &sdb, Assets &assets);
+    virtual void shutdown(StateDb &sdb);
+    virtual void update(StateDb &sdb, Assets &assets, Renderer &renderer, double deltaTimeInS);
+
+private:
+    COMMON_DISABLE_COPY(Sensors)
+};
+
+#endif
